c73c0fedccbccdf871a33a0353bbe00344e93b65
williamdahlke <williamgdahlke@gmail.com>
2023-08-15 22:17:16 -0300

    Add project files.

---
+++ Modulo1/wpf3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfAplicacionesEscritorio.Modulo1
+{
+    /// <summary>
+    /// Interaction logic for wpf3.xaml
+    /// </summary>
+    public partial class wpf3 : Window
+    {
+        public wpf3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stackPanel1.Resources.Add("Brush2", new SolidColorBrush(Colors.Brown));
+        }
+    }
+}
---
+++ Modulo1/wpf5.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfAplicacionesEscritorio.Modulo1
+{
+    /// <summary>
+    /// Interaction logic for wpf5.xaml
+    /// </summary>
+    public partial class wpf5 : Window
+    {
+        public wpf5()
+        {
+            InitializeComponent();
+        }
+
+        private void el1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            (grid1.Resources["sb1"] as Storyboard)?.Begin();
+        }
+    }
+}
